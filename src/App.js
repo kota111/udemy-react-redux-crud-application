51882b5d9a0684f1a1ff22acbc8b52051618a77c
kotakatahira <koutakatahira0@gmail.com>
2020-11-08 15:32:30 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import React,{Component} from 'react';
+import React from 'react';
 
-class App extends Component{
-  render(){
-    return <input type="text" onChange={()=>{console.log("i'm clicked")}}/>
-  }
+// class App extends Component{
+//   render(){
+//     return <input type="text" onChange={()=>{console.log("i'm clicked")}}/>
+//   }
+// }
+
+const App = ()=>{
+  return <div>hello</div>
 }
 export default App;
