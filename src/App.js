27f3635c47bf5ae6c5aac0a2552a3db55dae63fc
kotakatahira <koutakatahira0@gmail.com>
2020-11-08 18:14:43 +0900

    learn props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
-// class App extends Component{
-//   render(){
-//     return <input type="text" onChange={()=>{console.log("i'm clicked")}}/>
-//   }
-// }
-
 const App = ()=>{
-  return <div>hello</div>
+  const profiles = [
+    {name:"taro",age:335},
+    {name:"tadro",age:33},
+    {name:"tadsro",age:333},
+    {name:"tadsro"},
+    {age:333},
+  ]
+  return(
+      <div>
+        {
+          profiles.map((profile,index)=>{
+            return <User name={profile.name} age={profile.age} key={index}/>
+          })
+        }
+      </div>
+  )
+}
+const User = (props) =>{
+  return <div>{props.name} {props.age}</div>
+}
+User.defaultProps ={
+  age:1,
+  name:"nanashi",
 }
 export default App;
